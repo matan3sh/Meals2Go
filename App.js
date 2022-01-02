d381d59320a7946d4241fe19f4e53a1bb7c1f149
Matan Shaviro <matan3sh@gmail.com>
2022-01-02 20:07:49 +0200

    Integrate react-native-paper

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { Searchbar } from "react-native-paper";
 
 export default function App() {
   return (
     <>
       <SafeAreaView style={styles.container}>
         <View style={styles.search}>
-          <Text>search</Text>
+          <Searchbar placeholder="Search" />
         </View>
         <View style={styles.list}>
           <Text>list</Text>
@@ -25,7 +26,6 @@ const styles = StyleSheet.create({
   },
   search: {
     padding: 16,
-    backgroundColor: "green",
   },
   list: {
     flex: 1,
