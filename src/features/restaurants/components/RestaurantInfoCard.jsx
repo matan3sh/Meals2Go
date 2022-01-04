6b4c4753d1ef824e53dd5c1a17bce06e6c4269f6
Matan Shaviro <matan3sh@gmail.com>
2022-01-04 20:34:27 +0200

    Continue improving RestaurantInfoCard component

--- src/features/restaurants/components/RestaurantInfoCard.jsx
+++ src/features/restaurants/components/RestaurantInfoCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { Card } from "react-native-paper";
 
 const RestaurantCard = styled(Card)`
@@ -12,9 +12,19 @@ const RestaurantCardCover = styled(Card.Cover)`
   background-color: ${({ theme }) => theme.colors.bg.primary};
 `;
 
+const Address = styled(Text)`
+  font-family: ${({ theme }) => theme.fonts.body};
+  font-size: ${({ theme }) => theme.fontSizes.caption};
+`;
+
 const Title = styled(Text)`
-  padding: ${({ theme }) => theme.space[3]};
+  font-family: ${({ theme }) => theme.fonts.heading};
+  font-size: ${({ theme }) => theme.fontSizes.body};
   color: ${({ theme }) => theme.colors.ui.primary};
+`;
+
+const Info = styled(View)`
+  padding: ${({ theme }) => theme.space[3]};
 `;
 
 const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -33,7 +43,10 @@ const RestaurantInfoCard = ({ restaurant = {} }) => {
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
