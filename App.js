ccd5b253d829cf322a7b5b10ebc73ce2ff33ade5
Matan Shaviro <matan3sh@gmail.com>
2022-01-07 08:26:18 +0200

    Implement react-native tab navigation

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { Text } from "react-native";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import {
   useFonts as useOswald,
@@ -9,7 +12,21 @@ import {
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 import { theme } from "./src/infrastructure/theme";
+import { SafeArea } from "./src/components/utility/SafeArea";
 import RestaurantsScreen from "./src/features/restaurants/screens/RestaurantsScreen";
+
+const Tab = createBottomTabNavigator();
+
+const SettingsScreen = () => (
+  <SafeArea>
+    <Text>Settings Screen</Text>
+  </SafeArea>
+);
+const MapScreen = () => (
+  <SafeArea>
+    <Text>Map Screen</Text>
+  </SafeArea>
+);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({ Oswald_400Regular });
@@ -22,7 +39,13 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <NavigationContainer>
+          <Tab.Navigator screenOptions={{ headerShown: false }}>
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+            <Tab.Screen name="Map" component={MapScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
--- src/features/restaurants/screens/RestaurantsScreen.jsx
+++ src/features/restaurants/screens/RestaurantsScreen.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import styled from "styled-components/native";
-import { SafeAreaView, StatusBar, FlatList } from "react-native";
+import { FlatList } from "react-native";
 import { Searchbar } from "react-native-paper";
 
+import { SafeArea } from "../../../components/utility/SafeArea";
 import RestaurantInfoCard from "../components/RestaurantInfoCard";
-
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-`;
 
 const SearchContainer = styled.View`
   padding: ${({ theme }) => theme.space[3]};
