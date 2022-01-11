46ed1c21a687846baaef6be4ad8887ad53cac962
Matan Shaviro <matan3sh@gmail.com>
2022-01-11 20:20:36 +0200

    Retrive data from fake api through context

--- src/features/restaurants/screens/RestaurantsScreen.jsx
+++ src/features/restaurants/screens/RestaurantsScreen.jsx
@@ -1,8 +1,9 @@
-import React, { useContext } from "react";
+import React from "react";
 import styled from "styled-components/native";
-import { FlatList } from "react-native";
-import { Searchbar } from "react-native-paper";
-import { RestaurantsContext } from "../../../services/restaurants/restaurantsContext";
+import { FlatList, View } from "react-native";
+import { Searchbar, ActivityIndicator, Colors } from "react-native-paper";
+
+import { useRestaurants } from "../../../services/restaurants/restaurantsContext";
 import { SafeArea } from "../../../components/utility/SafeArea";
 import RestaurantInfoCard from "../components/RestaurantInfoCard";
 
@@ -14,18 +15,34 @@ const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: { padding: 16 },
 })``;
 
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+
+const LoadingContainer = styled(View)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
 const RestaurantsScreen = () => {
-  const restaurantsContext = useContext(RestaurantsContext);
-  console.log(restaurantsContext);
+  const { restaurants, isLoading, error } = useRestaurants();
+  console.log(restaurants);
 
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={Colors.blue300} />
+        </LoadingContainer>
+      )}
+
       <SearchContainer>
         <Searchbar placeholder="Search" />
       </SearchContainer>
       <RestaurantList
-        data={restaurantsContext.restaurants}
-        renderItem={() => <RestaurantInfoCard />}
+        data={restaurants}
+        renderItem={({ item }) => <RestaurantInfoCard restaurant={item} />}
         keyExtractor={(item) => item.name}
       />
     </SafeArea>
--- src/services/restaurants/restaurantsContext.js
+++ src/services/restaurants/restaurantsContext.js
@@ -1,12 +1,43 @@
-import React, { useState, createContext, useEffect, useMemo } from "react";
+import React, {
+  useState,
+  createContext,
+  useEffect,
+  useMemo,
+  useContext,
+} from "react";
 
 import { restaurantsRequest, restaurantsTransform } from "./restaurantsService";
 
 export const RestaurantsContext = createContext();
+export const useRestaurants = () => useContext(RestaurantsContext);
 
 export const RestaurantsContextProvider = ({ children }) => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const retriveRestaurants = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      restaurantsRequest()
+        .then(restaurantsTransform)
+        .then((results) => {
+          setIsLoading(false);
+          setRestaurants(results);
+        })
+        .catch((err) => {
+          setIsLoading(false);
+          setError(err);
+        });
+    }, 2000);
+  };
+
+  useEffect(() => {
+    retriveRestaurants();
+  }, []);
+
   return (
-    <RestaurantsContext.Provider value={{ restaurants: [1, 2, 3] }}>
+    <RestaurantsContext.Provider value={{ restaurants, isLoading, error }}>
       {children}
     </RestaurantsContext.Provider>
   );
