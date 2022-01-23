9368176cc5693d0dbfa2b2b466e7f85c4f559cab
Matan Shaviro <matan3sh@gmail.com>
2022-01-23 23:00:51 +0200

    Start integrate firbase authentication

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "react-native-gesture-handler";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
@@ -16,13 +16,45 @@ import { RestaurantsContextProvider } from "./src/services/restaurants/restauran
 import { LocationContextProvider } from "./src/services/location/locationContext";
 import { FavouritesContextProvider } from "./src/services/favourites/favouritesContext";
 
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAgZsx8NC6ue_0grk6X9CnUxZIzU7pp1oQ",
+  authDomain: "mealstogo-bde21.firebaseapp.com",
+  projectId: "mealstogo-bde21",
+  storageBucket: "mealstogo-bde21.appspot.com",
+  messagingSenderId: "826740415651",
+  appId: "1:826740415651:web:53f4eab39ad29019413316",
+};
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword("matan3sh@gmail.com", "123456")
+        .then((user) => {
+          setIsAuthenticated(true);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }, 2000);
+  }, []);
+
   const [oswaldLoaded] = useOswald({ Oswald_400Regular });
   const [latoLoaded] = useLato({ Lato_400Regular });
 
   if (!oswaldLoaded || !latoLoaded) {
     return null;
   }
+
+  if (!isAuthenticated) return null;
 
   return (
     <>
